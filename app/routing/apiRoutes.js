0cdd0c721c4727e64a09b63bdcb44954b8bf027b
tawny francis <tawnyfrancis@gmail.com>
2019-11-22 14:39:58 -0500

    updated api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,23 +11,31 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        var bestMatch = {
-            name:"",
-            photo:"",
-            friendDifference: Infinity
-        };
-        var userData = req.body;
-        var userScores = userData.scores;
-        var totalDifference = 
-        //for loop var to store count friend object- 
-            //--2nd- 1 friends particular score
-                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore))
-            if(totalDifference <= bestMatch.friendDifference) {
-                //
-                
-            } 
-        friends.push(userData)
-        res.json(bestMatch)
+        function compareScores(currentUserScore, currentFriendScore) {
+            var totalDifference = 0;
+            for (var i = 0; i < currentUserScore.length; i++) {
+                totalDifference += Math.abs(parseInt(currentUserScore[i]) - parseInt(currentFriendScore[i]));
+            }
+            return totalDifference;
+        }
+
+        var bestMatch = 40;
+        for (var i = 0; i < friends.length; i++) {
+            totalDifference = compareScores(req.body.scores, friends[i].scores)
+            if (totalDifference < bestMatch) {
+                bestMatch = totalDifference;
+                indexMatch = i;
+            }
+        }
+        
+       var userMatch = {
+            name: friends[indexMatch].name,
+            photo: friends[indexMatch].photo
+        }
+
+        friends.push(req.body);
+        res.json(userMatch);
+        console.log(userMatch);
     });
 
 }
